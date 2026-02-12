0496519b0bfc7e1da85aa425e0517b2bd184aa9f
DominicSchmidinger <kas240614@spengergasse.at>
2026-02-12 17:28:22 +0100

    exercise 10

--- Webdesign/Das bin ich/Java_Script/script.js
+++ Webdesign/Das bin ich/Java_Script/script.js
@@ -54,23 +54,95 @@ let num = prompt("number?");
 
 let nume = Number(num)
 
-alert(nume % 2 === 0? "Even" : "Odd")
-    
+if (nume === 0 || nume === null) {
+    alert("keine nummer eingegeben");
+}
+else
+    alert(nume % 2 === 0 ? "Even" : "Odd");
+
+
 //exercise 6
 
 let loginButton = document.getElementById("loginButton");
 
-loginButton.addEventListener("click", function() {
+loginButton.addEventListener("click", function () {
 
-let usernameInput = document.getElementById("username");
-let passwordInput = document.getElementById("password");
+    let usernameInput = document.getElementById("username");
+    let passwordInput = document.getElementById("password");
 
-let username = usernameInput.value;
-let password = passwordInput.value;
+    let username = usernameInput.value;
+    let password = passwordInput.value;
 
-if (username !== "admin" && password.length < 8 && username.length < 5) { 
-    alert("Invalid username and password22");
-}else {
-        alert("Login successful");
+    if (username !== "admin" || password.length < 8 || username.length < 5) {
+        alert("Login sucessfull");
+    } else {
+        alert("Invalid username or password");
     }
 });
+
+// exercise 7
+let username = prompt("whats your name?")
+let usertheme = prompt("choose a theme?")
+let userlanguage = prompt("ehat do you speak?")
+
+userNamefinal = username ?? "Guest"
+userThemefinal = usertheme ?? "light"
+userLanguagefinal = userlanguage ?? "en"
+
+console.log(userNamefinal, userThemefinal, userLanguagefinal);
+
+//exercise 8
+
+let i = 0;
+
+
+while (i <= 20) {
+    if (i % 2 == 0)
+        console.log("gerade zahlen: ", i)
+    i++
+}
+console.log("ende")
+
+
+for (let i = 0; i < 20; i++) {
+    if (i % 2)
+        console.log("ungerade zahlen: ", i)
+}
+console.log("ende")
+console.log("")
+console.log("ich liebe fizzbuzz")
+
+//exercise 9 
+
+for (let i = 0; i <= 30; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz")
+    }
+    else if (i % 3 == 0) {
+        console.log("Fizz")
+    } else if (i % 5 == 0) {
+        console.log("Buzz")
+    }
+    else {
+        console.log(i)
+    }
+}
+
+//exercise 10
+loginButton = document.getElementById("loginButton");
+usernameInput = document.getElementById("username");
+
+loginButton.addEventListener("click", function () {
+    let userName = usernameInput.value;
+    event.preventDefault();
+    showMassage(userName);
+});
+
+function showMassage(userName) {
+    alert("Hello " + userName);
+}
+
+//
+
+
+
