be7740f947d599da879e7c19796e92673c9cb815
DominicSchmidinger <kas240614@spengergasse.at>
2026-05-11 03:35:47 +0200

    ahhhhh

---
+++ Webdesign/Nur so/uebung.js
@@ -0,0 +1,44 @@
+const alter = 22;
+const name = "Dominic";
+
+function begruessung() {
+    console.log("Hallo, ich heiße " + name + " und ich bin " + alter + " Jahre alt.");
+}
+begruessung();
+
+alert("Hallo, ich heiße " + name + " und ich bin " + alter + " Jahre alt.");
+
+const alter2 = prompt("Wie alt bist du?");
+alert("Du bist " + alter2 + " Jahre alt.");
+
+const essen = ["Pizza", "Burger", "Sushi"];
+console.log(essen[1]); // Burger
+
+essen.push("pasta")
+
+const zahlen = [1,2,3,4,5];
+zahlen.forEach((zahl) => {
+    console.log(zahl);
+});
+
+const schüler = {
+    name: "Anna",
+    alter: 20,
+    klasse: "12A"
+};
+console.log(schüler.name); // Anna
+
+schüler.alter = 21;
+console.log(schüler.alter); // 21
+
+Text.innerText = "Willkommen";
+
+btn.onclick = () => {
+    console.log("Button wurde geklickt!");
+}
+
+const input = document.getElementById("input");
+
+addEventListener("input", () => {
+    console.log("Eingabe: ", input.value);
+})
